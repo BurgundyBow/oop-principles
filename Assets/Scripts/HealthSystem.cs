d1e41d77a1ceefed9d7e02347b33cb6356ef342e
BurgundyBow <peppermint.nyria@gmail.com>
2023-05-19 18:07:36 +0100

    done everything except menu
    added getters and setters and used abstraction

---
+++ Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+
+    private float _health;
+    [SerializeField] private float healthReadCounter;
+
+    public float Health
+    {
+        get
+        {
+            healthReadCounter++;
+            return _health;
+        }
+        private set
+        {
+            _health = Mathf.Clamp(value, 0, 10);
+            slider.value = _health;
+        }
+    }
+
+    public void TakeDamage(float _amount = 1)
+    {
+        Health = Health - _amount;
+    }
+
+    public void Heal(float _amount = 1)
+    {
+        Health = Health + _amount;
+    }
+}
--- Assets/Scripts/Shapes/Cube.cs
+++ Assets/Scripts/Shapes/Cube.cs
@@ -1,9 +1,19 @@
+using UnityEngine;
 public class Cube : Shape
 {
+    private float scale;
+    [SerializeField] HealthSystem healthSystem;
     public override void DisplayText()
     {
         base.DisplayText();
-       // displayText.transform.
+        scale = Random.Range(0.0f, 3.0f);
+        displayText.transform.localScale = new Vector3(scale, scale, scale);
+
+        if (healthSystem.Health > 5)
+        {
+            healthSystem.TakeDamage(5);
+        }
        // make this random scale
+   
     }
 }
